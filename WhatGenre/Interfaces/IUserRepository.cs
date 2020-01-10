4ff9792d86ce7c0de5b8d944c5cd7808d2ed2031
mnazir <monazir1001@gmail.com>
2020-01-10 00:35:10 +0000

    Created Repository Layer with interface

---
+++ WhatGenre/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WhatGenre.Models;
+
+namespace WhatGenre.Interfaces
+{
+    public interface IUserRepository
+    {
+        User GetById(int id);
+        List<User> GetUsers();
+        
+    }
+}
---
+++ WhatGenre/Repositories/UserRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using WhatGenre.Interfaces;
+using WhatGenre.Models;
+
+namespace WhatGenre.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+
+        private readonly WhatGenreContext _context;
+
+        public UserRepository(WhatGenreContext context)
+        {
+            _context = context;
+        }
+
+        public User GetById(int id)
+        {
+            return _context.Users.Find(id);
+        }
+
+        public List<User> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
