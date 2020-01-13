3a5cd59e54480a602656cf23b62933f96eb04b4b
mnazir <monazir1001@gmail.com>
2020-01-13 00:42:54 +0000

    Added user service and relevant interface.

---
+++ WhatGenre/Interfaces/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WhatGenre.Models;
+
+namespace WhatGenre.Interfaces
+{
+    public interface IUserService
+    {
+
+        public User GetUserById(int id);
+
+        public List<User> GetAllUsers();
+
+    }
+}
---
+++ WhatGenre/Services/UserService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WhatGenre.Interfaces;
+using WhatGenre.Models;
+
+namespace WhatGenre.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository userRepository;
+        
+        public UserService(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public User GetUserById(int id)
+        {
+            return userRepository.GetById(id);
+        }
+
+        public List<User> GetAllUsers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
