35def046ce0c6b817b8e65773bfb857d394770ae
Raihaan <raihaan.nazir@software2.com>
2020-01-31 09:20:35 +0000

    modified postcomment with keys for complex relationship

--- ApplicationCore/Entities/PostComment.cs
+++ ApplicationCore/Entities/PostComment.cs
@@ -1,18 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace ApplicationCore.Entities
 {
     public class PostComment
     {
+        [Key, Column(Order = 0)]
         public int PostId { get; set; }
         public Post Post { get; set; }
 
+        [Key, Column(Order = 1)]
         public int CommentId { get; set; }
         public Comment Comment { get; set; }
 
+        [ForeignKey("UserId")]
         public int UserId { get; set; }
+        public User User { get; set; }
 
     }
 }
