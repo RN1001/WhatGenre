cf9cd6c9edf5a5684b6760948feab754a0c4f93b
M Nazir <monazir1001@gmail.com>
2020-07-24 01:47:42 +0100

    DB context, also used for identity services.

--- Infrastructure/Data/WhatGenreContext.cs
+++ Infrastructure/Data/WhatGenreContext.cs
@@ -1,18 +1,18 @@
 ﻿using ApplicationCore.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 namespace Infrastructure.Data
 {
-    public class WhatGenreContext : DbContext
+    public class WhatGenreContext : IdentityDbContext<User, ApplicationRole, string>
     {
         public WhatGenreContext(DbContextOptions<WhatGenreContext> options) : base(options)
         {
-            Database.EnsureDeleted();
-            Database.EnsureCreated();
+           
         }
 
-        public DbSet<User> Users { get; set; }
         public DbSet<Address> Addresses { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Post> Posts { get; set; }
@@ -21,6 +21,8 @@ namespace Infrastructure.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<User>()
               .HasMany(a => a.Addresses)
               .WithOne(u => u.User);
@@ -32,7 +34,7 @@ namespace Infrastructure.Data
 
             modelBuilder.Entity<PostComment>()
                 .HasOne(p => p.Post)
-                .WithMany(p => p.PostComments)
+                .WithMany(pc => pc.PostComments)
                 .HasForeignKey(p => p.PostId);
 
             modelBuilder.Entity<PostComment>()
