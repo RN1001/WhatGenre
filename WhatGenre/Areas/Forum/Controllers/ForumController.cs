ee9f31211f343f9d3490a8f12c20ba67ab5ec119
M Nazir <monazir1001@gmail.com>
2020-07-26 23:47:43 +0100

    Created Forum and Store controllers with Index page and additional empty pages.

---
+++ WhatGenre/Areas/Forum/Controllers/ForumController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WhatGenre.Areas.Forum.Controllers
+{
+    [Area("Forum"), Route("Forum")]
+    public class ForumController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpGet, Route("Add")]
+        public IActionResult AddPost()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ WhatGenre/Areas/Store/Controllers/StoreController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WhatGenre.Areas.Store.Controllers
+{
+    [Area("Store"), Route("Store")]
+    public class StoreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpGet, Route("Products")]
+        public IActionResult Products()
+        {
+            return View("Products");
+        }
+    }
+}
