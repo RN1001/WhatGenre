a3255c6643b138153578119f32341bee1ef1dd6d
M Nazir <monazir1001@gmail.com>
2020-07-26 23:45:07 +0100

    Modified Post model, added Post interfaces, service and repo

---
+++ Infrastructure/Data/PostRepository.cs
@@ -0,0 +1,19 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Data
+{
+    public class PostRepository : EfRepository<Post>, IPostRepository
+    {
+        private readonly WhatGenreContext context;
+
+        public PostRepository(WhatGenreContext whatGenreContext) : base(whatGenreContext)
+        {
+            context = whatGenreContext;
+        }
+
+    }
+}
