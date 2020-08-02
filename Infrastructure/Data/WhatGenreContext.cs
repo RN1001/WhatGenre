15db0ba2a2c94ec0660f8871830a8dd603f97f68
M Nazir <monazir1001@gmail.com>
2020-08-02 02:43:26 +0100

    Added Correct relationships between model entities

--- Infrastructure/Data/WhatGenreContext.cs
+++ Infrastructure/Data/WhatGenreContext.cs
@@ -16,9 +16,7 @@ namespace Infrastructure.Data
         public DbSet<Address> Addresses { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Post> Posts { get; set; }
-        public DbSet<PostComment> PostComments { get; set; }
-
-
+       
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
@@ -27,24 +25,18 @@ namespace Infrastructure.Data
               .HasMany(a => a.Addresses)
               .WithOne(u => u.User);
 
-            modelBuilder.Entity<Address>().ToTable("Address");
-            modelBuilder.Entity<Post>().ToTable("Post");
-            modelBuilder.Entity<Comment>().ToTable("Comment");
-            modelBuilder.Entity<PostComment>().HasKey(pc => new { pc.PostId, pc.CommentId });
+            modelBuilder.Entity<User>()
+                .HasMany(p => p.Posts)
+                .WithOne(u => u.User);
+
+            modelBuilder.Entity<User>()
+                .HasMany(c => c.Comments)
+                .WithOne(u => u.User);
 
             modelBuilder.Entity<Post>()
-                .Property(p => p.CreatedDate)
-                .HasDefaultValueSql("getdate()");
+                .HasMany(c => c.Comments)
+                .WithOne(p => p.Post);
 
-            modelBuilder.Entity<PostComment>()
-                .HasOne(p => p.Post)
-                .WithMany(pc => pc.PostComments)
-                .HasForeignKey(p => p.PostId);
-
-            modelBuilder.Entity<PostComment>()
-                .HasOne(c => c.Comment)
-                .WithMany(pc => pc.PostComments)
-                .HasForeignKey(c => c.CommentId);
 
         }
 
