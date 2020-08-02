c1db3a07d536986ee8c78487908f449e33955552
M Nazir <monazir1001@gmail.com>
2020-08-02 02:46:27 +0100

    Basic comment controller for comment actions

---
+++ WhatGenre/Areas/Forum/Controllers/CommentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApplicationCore.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WhatGenre.Areas.Forum.Controllers
+{
+    [Area("Forum")]
+    public class CommentController : Controller
+    {
+        private readonly IPostService postService;
+
+        private readonly ICommentService commentService;
+
+        public CommentController(IPostService postService, ICommentService commentService)
+        {
+            this.postService = postService;
+            this.commentService = commentService;
+        }
+
+        public IActionResult Create()
+        {
+            return View("");
+        }
+    }
+}
