f3d79875ce4105be5867299381fa1e958e3b9f21
M Nazir <monazir1001@gmail.com>
2020-09-09 10:34:39 +0100

    Added comments for all controller methods

--- WhatGenre/Areas/Forum/Controllers/ForumController.cs
+++ WhatGenre/Areas/Forum/Controllers/ForumController.cs
@@ -17,6 +17,12 @@ namespace WhatGenre.Areas.Forum.Controllers
         private readonly ICommentService commentService;
         private readonly UserManager<User> userManager;
 
+        /// <summary>
+        ///  Controller to initialise services
+        /// </summary>
+        /// <param name="postService"></param>
+        /// <param name="commentService"></param>
+        /// <param name="userManager"></param>
         public ForumController(IPostService postService, ICommentService commentService, UserManager<User> userManager)
         {
             this.postService = postService;
@@ -24,6 +30,10 @@ namespace WhatGenre.Areas.Forum.Controllers
             this.userManager = userManager;
         }
 
+        /// <summary>
+        ///  gets all posts, displays on index
+        /// </summary>
+        /// <returns> returns the index view </returns>
         [HttpGet]
         public async Task<IActionResult> Index()
         {
@@ -31,6 +41,10 @@ namespace WhatGenre.Areas.Forum.Controllers
             return View("Post/Index", posts);
         }
 
+        /// <summary>
+        /// displays a create form
+        /// </summary>
+        /// <returns>returns a create form</returns>
         [Authorize]
         [HttpGet, Route("Create")]
         public IActionResult Create()
@@ -38,6 +52,13 @@ namespace WhatGenre.Areas.Forum.Controllers
             return View("Post/Create");
         }
 
+        /// <summary>
+        /// creates a new post
+        /// </summary>
+        /// <param name="post"></param>
+        /// <returns> a redirect if the post is successful, or returns the view
+        /// if their is errors on the form.
+        /// </returns>
         [Authorize]
         [ValidateAntiForgeryToken]
         [HttpPost, Route("Create")]
@@ -56,6 +77,11 @@ namespace WhatGenre.Areas.Forum.Controllers
             return View("Post/Create");
         }
 
+        /// <summary>
+        /// details page that displays the post with all its comments associated to the post ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>returns a view with post and comment data </returns>
         [Authorize]
         [HttpGet, Route("Details/{id}")]
         public async Task<IActionResult> Details(int? id)
@@ -72,6 +98,11 @@ namespace WhatGenre.Areas.Forum.Controllers
             return View("Post/Details", posts);
         }
 
+        /// <summary>
+        /// returns edit form with current post data by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>returns edit view</returns>
         [Authorize]
         [HttpGet, Route("Edit/{id}")]
         public async Task<IActionResult> Edit(int id)
@@ -81,6 +112,13 @@ namespace WhatGenre.Areas.Forum.Controllers
             return View("Post/Edit", post);
         }
 
+        /// <summary>
+        /// form post gets the id, and the current created date and if it is valid
+        /// uses the service to save changes.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="post"></param>
+        /// <returns>redirect after successful service function or view if there is an error</returns>
         [Authorize]
         [ValidateAntiForgeryToken]
         [HttpPost, Route("Edit/{id}")]
@@ -99,6 +137,11 @@ namespace WhatGenre.Areas.Forum.Controllers
             return View("Post/Edit");
         }
 
+        /// <summary>
+        /// returns a delete form with post data
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>delete form view</returns>
         [Authorize]
         [HttpGet, Route("Delete/{id}")]
         public async Task<IActionResult> Delete(int? id)
@@ -108,6 +151,12 @@ namespace WhatGenre.Areas.Forum.Controllers
             return View("Post/Delete", post);
         }
 
+        /// <summary>
+        /// deletes post model if the model is not null and is in the db.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="post"></param>
+        /// <returns> redirect if successful deletion, otherwise returns a badrequest </returns>
         [Authorize]
         [HttpPost, Route("Delete/{id}")]
         public async Task<IActionResult> Delete(int? id, Post post)
